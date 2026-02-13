c223899c9e5ffc2a11b1d38b57c8ef826cad1856
Suraj Yadav <surajy1303@gmail.com>
2026-02-14 01:16:03 +0530

    Time: 7 ms (55.61%) | Memory: 47 MB (21.62%) - LeetSync

--- 1242-matrix-block-sum/matrix-block-sum.java
+++ 1242-matrix-block-sum/matrix-block-sum.java
@@ -2,24 +2,30 @@ class Solution {
     public int[][] matrixBlockSum(int[][] mat, int k) {
         int n=mat.length;
         int m=mat[0].length;
-        int[][] ans =new int[n][m];
+        int[][] pre =new int[n+1][m+1];//to store the last one
+
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                pre[i][j]=pre[i-1][j]+pre[i][j-1]-pre[i-1][j-1]+mat[i-1][j-1];
+            }
+        }
+
+        int[][] ans=new int[n][m];
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                add(i,j,k,mat,ans);
+                int r1=Math.max(0,i-k);//leftmost
+                int c1=Math.max(0,j-k);
+                int r2=Math.min(n-1,i+k);
+                int c2=Math.min(m-1,j+k);//rightmost
+
+                r1++;r2++;c1++;c2++;//to fix the indexing error, we created while giving values to it
+
+                
+                ans[i][j]=pre[r2][c2]-pre[r1-1][c2]-pre[r2][c1-1]+pre[r1-1][c1-1];
             }
         }
         return ans;
-    }
-
-    public void add(int r,int c,int k,int[][] mat,int[][] ans){
-        int sum=0;
-        for(int i=r-k;i<=r+k;i++){
-            for(int j=c-k;j<=c+k;j++){
-                if(i<0 || j<0 || i>=mat.length || j>=mat[0].length) continue;
-                sum+=mat[i][j];
-            }
-        }
-        ans[r][c]=sum;
     }
 }
