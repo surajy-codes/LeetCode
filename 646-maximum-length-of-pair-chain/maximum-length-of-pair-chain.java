353cb72a612ce06f6cf98bb10fcca1c4f7b152a8
Suraj Yadav <surajy1303@gmail.com>
2026-02-19 00:04:34 +0530

    Time: 9 ms (94.21%) | Memory: 47.3 MB (31.75%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.java
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.java
@@ -3,15 +3,11 @@ class Solution {
         Arrays.sort(pairs,(a,b)->a[1]-b[1]);
         
         int ans=0;
+        int prev=Integer.MIN_VALUE;
         for(int i=0;i<pairs.length;i++){
-            int prev=Integer.MIN_VALUE;
-            int curr=0;
-            for(int j=i;j<pairs.length;j++){
-                if(prev<pairs[j][0]){
-                    prev=pairs[j][1];
-                    curr++;
-                    ans=Math.max(ans,curr);
-                }
+            if(prev<pairs[i][0]){
+                ans++;
+                prev=pairs[i][1];
             }
         }
         return ans;
